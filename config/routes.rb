5efe498691a7ee1612c37e9db664d1bb6977ffe6
YeinerCQ <ycarvajalq@gmail.com>
2022-10-16 13:49:32 -0500

    add notes to tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks
+  resources :tasks do
+    resources :notes, only: [:create], controller: 'tasks/notes'
+  end
+
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
