d64e2139d55dcf994b84d7218c4b70a8f3ea085a
YeinerCQ <ycarvajalq@gmail.com>
2022-10-16 21:40:50 -0500

    fix dependent destroy for participants

--- app/models/task.rb
+++ app/models/task.rb
@@ -16,7 +16,7 @@ class Task < ApplicationRecord
   belongs_to :category
   belongs_to :owner, class_name: 'User'
   has_many :participating_users, class_name: 'Participant'
-  has_many :participants, through: :participating_users, source: :user
+  has_many :participants, through: :participating_users, source: :user, dependent: :destroy
   has_many :notes
 
   accepts_nested_attributes_for :participating_users, reject_if: :all_blank, allow_destroy: true
